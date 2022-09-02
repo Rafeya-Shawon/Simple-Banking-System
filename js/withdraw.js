aba9c8c284d4c3a42c49a95db26e99403216b8cf
Rafeya Khandaker <rafeya.khandaker88@gmail.com>
2022-09-02 11:45:16 +0600

    error handeling done

--- js/withdraw.js
+++ js/withdraw.js
@@ -5,12 +5,19 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
 
     const withdraw = document.getElementById('withdraw');
     const withdrawShow = withdraw.innerText;
-    const withdrawShowFloat = parseFloat(withdrawShow);
-    withdraw.innerText = withdrawAmmountFloat + withdrawShowFloat;
 
     const currentBalance = document.getElementById('current-balance');
     const currentBalanceShow = currentBalance.innerText;
     const currentBalanceFloat = parseFloat(currentBalanceShow);
+
+    if(withdrawAmmountFloat> currentBalanceFloat){
+        alert('Insufficient Balance');
+        withdraField.value = '';
+        return;
+    }
+
+    const withdrawShowFloat = parseFloat(withdrawShow);
+    withdraw.innerText = withdrawAmmountFloat + withdrawShowFloat;
     currentBalance.innerText = currentBalanceFloat - withdrawAmmountFloat;
 
     withdraField.value = '';
